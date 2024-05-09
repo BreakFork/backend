124bfde362642b610699546dbfb026c70368a060
HRVB <h.boulangue@gmail.com>
2024-05-09 13:25:41 +0200

    Test POST OK

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const app = express();
+app.use(express.json()); // Permet de récuopérer le corps de la req en JSON
 
 // CORS 
 app.use((req, res, next) => {
@@ -8,6 +8,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.post('/api/auth/signup', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'SignUp OK !!!'
+    });
 });
 
 app.get('/api/books', (req, res, next) => {
